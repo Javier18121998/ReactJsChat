81f46a8f78d77e9f45ea16d9dbfdfee9ac2d6066
Javier18121998 <Javier.Marin.Garcia.Linkedin01@hotmail.com>
2023-04-30 00:55:57 -0600

    updated at 30 April 2023

--- src/Components/Principal.js
+++ src/Components/Principal.js
@@ -1,7 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+const style = {
+ primary:{
+  position: 'absolute',
+  left: '50%',
+  top: '50%',
+  transform: 'translate(-50%, -50%)'
+ },
+ title:{
+  color: '#fdfdfd',
+ }
+}
 
-export default function Principal({ onInputChange }) {
-    const [inputValue, setInputValue] = useState("");
+export default function Principal() {
+  const [inputValue, setInputValue] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const savedMessages = JSON.parse(localStorage.getItem("messages")) || [];
+    setMessages(savedMessages);
+  }, []);
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
@@ -9,18 +26,35 @@ export default function Principal({ onInputChange }) {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    localStorage.setItem("datos", JSON.stringify([...(JSON.parse(localStorage.getItem("datos")) || []), inputValue]));
-    onInputChange(inputValue);
+    const newMessage = { from: "Primary", text: inputValue };
+    localStorage.setItem("messages", JSON.stringify([...messages, newMessage]));
+    setMessages([...messages, newMessage]);
     setInputValue("");
   };
-
+  const handleClearMessages = () => {
+    localStorage.removeItem("messages");
+    setMessages([]);
+  };
   return (
-    <form onSubmit={handleSubmit}>
+    <div style={style.primary}>
+      <h3 style={style.title}>Primary</h3>
+      <div>
+        {messages.map((message, index) => (
+          <div key={index}>
+            <strong className="fw-semibold">{message.from}:</strong> <h6 className="fw-normal text-warning">{message.text}</h6>
+          </div>
+        ))}
+      </div>
+      <form onSubmit={handleSubmit}>
         <label>
-            <h5>Ingresa un dato:</h5>
-            <input type="text" value={inputValue} onChange={handleInputChange} />
+          <h5 className="fw-bolder text-info-emphasis">Ingresa un mensaje:</h5>
+          <input className="form-control opacity-75" type="text" value={inputValue} onChange={handleInputChange} />
         </label>
-        <button type="submit">Agregar</button>
-    </form>
+        <button type="submit" className="btn btn-success">Enviar</button>
+      </form>
+      {messages.length > 0 && (
+        <button className="btn btn-danger" onClick={handleClearMessages}>Eliminar todos los mensajes</button>
+      )}
+    </div>
   );
 }
--- src/Components/Secondary.js
+++ src/Components/Secondary.js
@@ -1,29 +1,63 @@
 import React, { useState, useEffect } from "react";
 
+const style = {
+  secondary:{
+   position: 'absolute',
+   left: '50%',
+   top: '50%',
+   transform: 'translate(-50%, -50%)'
+  },
+  title:{
+   color: '#fdfdfd',
+  }
+ }
+
 export default function Secondary() {
-  const [data, setData] = useState([]);
-  const [showData, setShowData] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    const datos = JSON.parse(localStorage.getItem("datos")) || [];
-    setData(datos);
+    const savedMessages = JSON.parse(localStorage.getItem("messages")) || [];
+    setMessages(savedMessages);
   }, []);
 
-  const handleClick = () => {
-    setShowData(true);
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
   };
 
-  const handleClearData = () => {
-    localStorage.removeItem("datos");
-    setData([]);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newMessage = { from: "Secondary", text: inputValue };
+    localStorage.setItem("messages", JSON.stringify([...messages, newMessage]));
+    setMessages([...messages, newMessage]);
+    setInputValue("");
+  };
+
+  const handleClearMessages = () => {
+    localStorage.removeItem("messages");
+    setMessages([]);
   };
 
   return (
-    <div>
-      <button onClick={handleClick}>Mostrar datos</button>
-      {showData && data.map((dato, index) => <p key={index}>{dato}</p>)}
-      {showData && data.length === 1 && (<button onClick={handleClearData}>Eliminar Elemento</button>)}
-      {showData && data.length > 1 && (<button onClick={handleClearData}>Eliminar Elementos</button>)}
+    <div style={style.secondary}>
+      <h3 style={style.title}>Secondary</h3>
+      <div>
+        {messages.map((message, index) => (
+          <div key={index}>
+            <strong className="fw-semibold">{message.from}:</strong> <h6 className="fw-normal text-warning">{message.text}</h6>
+          </div>
+        ))}
+      </div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <h5 className="fw-bolder text-info-emphasis">Ingresa un mensaje:</h5>
+          <input className="form-control opacity-75" type="text" value={inputValue} onChange={handleInputChange} />
+        </label>
+        <button type="submit" className="btn btn-success">Enviar</button>
+      </form>
+      {messages.length > 0 && (
+        <button className="btn btn-danger" onClick={handleClearMessages}>Eliminar todos los mensajes</button>
+      )}
     </div>
   );
 }
